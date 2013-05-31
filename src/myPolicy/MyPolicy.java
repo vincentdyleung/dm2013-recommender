d0644ef5f68055d0cca77b1693523620528540cb
Vincent Leung <vincentleung1991@gmail.com>
2013-06-01 00:50:41 +0200

    second attempt

--- src/myPolicy/MyPolicy.java
+++ src/myPolicy/MyPolicy.java
@@ -9,7 +9,8 @@ import org.jblas.DoubleMatrix;
 import org.jblas.Solve;
 
 public class MyPolicy implements ContextualBanditPolicy<User, Article, Boolean> {
-	public static final double ALPHA = 1.0;
+	public static final double DELTA = 0.01;
+	public static final double ALPHA = 1 + Math.sqrt(Math.log(2 / DELTA) / 2);
 	public static final int ARTICLE_COUNT = 271;
 	public static final int ARTICLE_FEAT_DIMEN = 6;
 	public static final int USER_FEAT_DIMEN = 6;
@@ -18,6 +19,7 @@ public class MyPolicy implements ContextualBanditPolicy<User, Article, Boolean> 
 	private DoubleMatrix[] matrixA;
 	private DoubleMatrix[] vectorB;
 	private DoubleMatrix userFeature;
+	private Random random;
 	
   // Here you can load the article features.
   public MyPolicy(String articleFilePath) {
@@ -26,6 +28,7 @@ public class MyPolicy implements ContextualBanditPolicy<User, Article, Boolean> 
   	ones = DoubleMatrix.ones(ARTICLE_FEAT_DIMEN);
   	identity = DoubleMatrix.diag(ones, ARTICLE_FEAT_DIMEN, ARTICLE_FEAT_DIMEN);
   	userFeature = new DoubleMatrix(USER_FEAT_DIMEN);
+  	random = new Random();
   }
 
   @Override
@@ -34,7 +37,7 @@ public class MyPolicy implements ContextualBanditPolicy<User, Article, Boolean> 
   		userFeature.put(i, 0, visitor.getFeatures()[i]);
   	}
   	double max = Double.NEGATIVE_INFINITY;
-  	int maxIndex = 0;
+  	int maxIndex = random.nextInt(possibleActions.size());
   	for (Article article : possibleActions) {
   		int id = article.getID() % ARTICLE_COUNT;
   		if (matrixA[id] == null) {
@@ -50,13 +53,16 @@ public class MyPolicy implements ContextualBanditPolicy<User, Article, Boolean> 
   			max = p;
   			maxIndex = possibleActions.indexOf(article);
   		}
-  		matrixA[id] = matrixA[id].add(userFeature.transpose().mmul(userFeature));
+  		matrixA[id] = matrixA[id].add(userFeature.mmul(userFeature.transpose()));
   	}
     return possibleActions.get(maxIndex);
   }
 
   @Override
   public void updatePolicy(User c, Article a, Boolean reward) {
+  	for (int i = 0; i < USER_FEAT_DIMEN; i++) {
+  		userFeature.put(i, 0, c.getFeatures()[i]);
+  	}
   	for (int i = 0; i < ARTICLE_COUNT; i++) {
   		if (vectorB[i] != null) {
   			if (reward) {
